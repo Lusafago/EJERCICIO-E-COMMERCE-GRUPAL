50f6764246a8c70b00498052c690f8d9fab5ebe2
Fani Casco <fanymar@live.com>
2024-09-20 17:45:17 -0300

    Carga de img, diseño de colores y responsivo

--- JS/main.js
+++ JS/main.js
@@ -57,3 +57,37 @@ document.addEventListener('DOMContentLoaded', function () {
 
     return esValido;
   }
+
+  /* */
+
+  window.onload = function() {
+    // Mostrar todas las categorías al cargar la página
+    var categorias = document.querySelectorAll('.oculto');
+    categorias.forEach(function(categoria) {
+        categoria.style.display = 'block';  // Mostrar todas las categorías
+    });
+
+    // Detectar si hay un hash en la URL (ej: #contenedor1)
+    var hash = window.location.hash;
+    if (hash) {
+        // Ocultar todas las categorías
+        categorias.forEach(function(categoria) {
+            categoria.style.display = 'none';
+        });
+
+        // Mostrar solo la categoría seleccionada
+        var categoriaSeleccionada = document.querySelector(hash);
+        if (categoriaSeleccionada) {
+            categoriaSeleccionada.style.display = 'block';
+
+            // Forzar el recargado de las imágenes en la categoría seleccionada
+            var imagenes = categoriaSeleccionada.querySelectorAll('img');
+            imagenes.forEach(function(imagen) {
+                var src = imagen.src;
+                imagen.src = '';  // Vaciamos el src temporalmente
+                imagen.src = src; // Recargamos la imagen forzadamente
+            });
+        }
+    }
+};
+
